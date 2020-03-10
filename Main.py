708534967edd0783ab4b0f71c4610574bfb6ff98
JMader8 <JMader8>
2020-03-10 08:38:35 -0500

    Sorting out syntax for branching paths, not a ton of progress.

--- Main.py
+++ Main.py
@@ -1,32 +1,10 @@
-#from Character import character
+# from Character import character
 
-#character1 = character("John", "Protagonist", "Mercenary", 21)
+# character1 = character("John", "Protagonist", "Mercenary", 21)
 
-#print(character1.__str__())
+# print(character1.__str__())
 
 global player
-global choice
-
-def intro():
-    global player
-    global choice
-    player = input("Welcome weary traveller. What's your name? ")
-    print(f"Hello, {player}. What is it you would like to do? We've got a few "
-          f"jobs available. Someone's gotta deal with the goblins, though they're a bit boring. (1)"
-          )
-    print("Then we have the alaskan bull worm (2) and the space lazer-eels (3). They're deadly, but need taking care of. ")
-    if input(choice) == 1:
-        goblin()
-    elif input(choice) == 2:
-        bullworm()
-    elif input(choice) == 3:
-        lazereel()
-
-
-
-
-
-intro()
 
 def goblin():
     print("The goblins, aye? Well, you have your reasons. I'll send ya over to em.")
@@ -35,6 +13,30 @@ def goblin():
 def bullworm():
     print("Ohh, the bullworm, eh? Good luck")
 
+
 def lazereel():
     print("You sir are the most ballsy of the adventurers yet. None have challenged the eels.")
+
+
+def intro():
+    global player
+    choice = ""
+    player = input("Welcome weary traveller. What's your name? ")
+    print(f"Hello, {player}. What is it you would like to do? We've got a few "
+          f"jobs available. Someone's gotta deal with the goblins, though they're a bit boring. (1)"
+          )
+    print(
+        "Then we have the alaskan bull worm (2) and the space lazer-eels (3). They're deadly, but need taking care of. ")
+
+
+    if input(choice) == "1":
+        return goblin()
+    elif choice == "2":
+        return bullworm()
+    elif choice == "3":
+        return lazereel()
+
+
+intro()
+
 
